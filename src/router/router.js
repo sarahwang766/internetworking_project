089923159e3507169b7b281d61e81b3ad8d41990
Xiaohan <51195467+sarahwang766@users.noreply.github.com>
2019-09-20 14:52:48 +1000

    fix create-task.jsx

--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Login from '../components/login/login.js'
 import Reg from '../components/reg/reg.js'
-import Task from './../views/task/task.jsx'
+import AdminPage from '../components/adminPage/adminPage.jsx'
 
 export default class AppRouter extends React.Component {
     constructor(props) {
@@ -16,7 +16,7 @@ export default class AppRouter extends React.Component {
                     <Route exact path="/" component={Login}></Route>
                     <Route path="/login" component={Login}></Route>
                     <Route path="/reg" component={Reg}></Route>
-                    <Route path="/task" component={Task}></Route>
+                    <Route path="/adminPage" component={AdminPage}></Route>
                 </Switch>
             </BrowserRouter>
         )
